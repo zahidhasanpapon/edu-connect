e19a4b02cb71ffab3ac73cd5655ff02e770e6a41
praava-zahid <123307051+praava-zahid@users.noreply.github.com>
2023-05-10 00:26:30 +0600

    add application.yml and test local Postgre connection

---
+++ src/test/java/com/example/education/educonnect/EduconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.education.educonnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
