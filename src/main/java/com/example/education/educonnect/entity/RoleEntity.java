2deb9e7479fca4e92bfd709c64f4a0a7b1d5d63f
praava-zahid <123307051+praava-zahid@users.noreply.github.com>
2023-05-12 23:12:30 +0600

    added role entity

---
+++ src/main/java/com/example/education/educonnect/entity/RoleEntity.java
@@ -0,0 +1,32 @@
+package com.example.education.educonnect.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "role")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
