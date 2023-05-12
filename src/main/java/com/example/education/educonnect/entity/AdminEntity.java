9a9d0cd47e75761f0b719b257163989b6f7aa518
praava-zahid <123307051+praava-zahid@users.noreply.github.com>
2023-05-12 23:10:20 +0600

    added admin entity

---
+++ src/main/java/com/example/education/educonnect/entity/AdminEntity.java
@@ -0,0 +1,36 @@
+package com.example.education.educonnect.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "admin")
+public class AdminEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
+
+
+
+
