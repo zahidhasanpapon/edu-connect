8659e0c81d17a3f226ac7d4e6d816a188addcd51
praava-zahid <123307051+praava-zahid@users.noreply.github.com>
2023-05-12 23:12:13 +0600

    added department entity

---
+++ src/main/java/com/example/education/educonnect/entity/DepartmentEntity.java
@@ -0,0 +1,31 @@
+package com.example.education.educonnect.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "department")
+public class DepartmentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
